c2b0f63dd379fea5063fea56334a97beb9028dce
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-02-20 08:38:07 -0500

    books section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Nav from "./components/Nav";
 import Landing from "./components/Landing";
 import Highlights from './components/Highlights';
+import Featured from './components/Featured';
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Nav />
       <Landing />
       <Highlights />
+      <Featured />
     </div>
   );
 }
