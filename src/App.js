1c891f14a6fdf00b08e66f2d3ac219424a0c641d
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-02-20 07:31:45 -0500

    highlight component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Nav from "./components/Nav";
+import Landing from "./components/Landing";
+import Highlights from './components/Highlights';
 
 function App() {
   return (
-    <div>
+    <div className="App">
       <Nav />
+      <Landing />
+      <Highlights />
     </div>
   );
 }
