e6f31c1587e7091b6366ae9dd37c2067c802385f
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-02-22 16:09:09 -0500

    h

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Nav from "./components/Nav";
-import Landing from "./components/Landing";
-import Highlights from './components/Highlights';
-import Featured from './components/Featured';
-import Discounted from './components/Discounted';
-import Explore from './components/Explore';
 import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Nav />
-      <Landing />
-      <Highlights />
-      <Featured />
-      <Discounted />
-      <Explore />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Home />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
