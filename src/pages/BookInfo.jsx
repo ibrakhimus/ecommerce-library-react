51cde8922303902a825175cccf39ee26f7a6dad9
Ibrahim <randizzys@gmail.com>
2023-02-27 16:52:19 -0500

    bookinfo

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -8,10 +8,18 @@ export default function BookInfo( { books }) {
         <main id="books__main">
             <div className="books__container">
                 <div className="row">
-                    <div className="book__selecter--top">
+                    <div className="book__selected--top">
                         <Link to="/books" className="book__link">
                             <FontAwesomeIcon icon="arrow-left" />
                         </Link>
+                        <Link to="/books" className="book__link">
+                            <h2 className='book__selected--title--top'>Books</h2>
+                        </Link>
+                    </div>
+                    <div className="book__selected">
+                        <figure className="book__selected--figure">
+                            img.book__selected--ima
+                        </figure>
                     </div>
                 </div>
             </div>
