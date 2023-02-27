7b6e2767858edafa8d9cececd206149d22c84b5c
Ibrahim <randizzys@gmail.com>
2023-02-27 16:26:10 -0500

    f

--- src/pages/BookInfo.jsx
+++ src/pages/BookInfo.jsx
@@ -1,4 +1,5 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { Link } from 'react-router-dom'
 import React from 'react'
 
 export default function BookInfo( { books }) { 
