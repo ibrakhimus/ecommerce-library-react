fab73d9e51eed83b1f1c31ec67691f7c2361fae6
Ibrahim <79383289+ibrakhimus@users.noreply.github.com>
2023-03-03 12:11:41 -0500

    cart html and link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import Books from "./pages/Books";
 import { books } from "./data"
 import BookInfo from "./pages/BookInfo";
+import Cart from "./pages/Cart";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books books={books} />} />
           <Route  path="/books/:id" element={<BookInfo books={books} />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </div>
     </Router>
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -6,7 +6,16 @@ export default function Cart() {
         <main id="books__main">
             <div className="books__container">
                 <div className="row">
-                    <div className="book__selected--top"></div>
+                    <div className="book__selected--top">
+                        <h2 className="cart__title">Cart</h2>
+                    </div>
+                    <div className="cart">
+                        <div className="cart__header">
+                            <div className="cart__book">Book</div>
+                            <div className="cart__quantity">Quantity</div>
+                            <div className="cart__total">Price</div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </main>
